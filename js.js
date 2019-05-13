651011541e79401124f77450db81e05888f42188
inna <innabelt@gmail.com>
2019-05-13 21:55:24 +0300

    js

--- js.js
+++ js.js
@@ -1,60 +1,60 @@
-// let myIndex = 0;
-// carousel();
+let myIndex = 0;
+carousel();
 
-// function carousel() {
-//   let i;
-//   let x = document.getElementsByClassName("slide");
-//   for (i = 0; i < x.length; i++) {
-//   	x[i].className = 'slide';
-//     x[i].style.display = "none";  
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) {myIndex = 1}   
-//   x[myIndex-1].className = 'slide  showing'; 
-//   x[myIndex-1].style.display = "block";  
-//   setTimeout(carousel, 3000); // Change image every 3 seconds
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("slide");
+  for (i = 0; i < x.length; i++) {
+  	x[i].className = 'slide';
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}   
+  x[myIndex-1].className = 'slide  showing'; 
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 3000); // Change image every 3 seconds
+}
+
+
+// /* Индекс слайда по умолчанию */
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+// /* Функция увеличивает индекс на 1, показывает следующй слайд*/
+// function plusSlide() {
+//     showSlides(slideIndex += 1);
 // }
 
+// /* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+// function minusSlide() {
+//     showSlides(slideIndex -= 1);  
+// }
 
-/* Индекс слайда по умолчанию */
-let slideIndex = 1;
-showSlides(slideIndex);
+// /* Устанавливает текущий слайд */
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-/* Функция увеличивает индекс на 1, показывает следующй слайд*/
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
-
-/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
-function minusSlide() {
-    showSlides(slideIndex -= 1);  
-}
-
-/* Устанавливает текущий слайд */
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-/* Основная функция слайдера */
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("item");
-    let dots = document.getElementsByClassName("slider-dots_item");
-    if (n > slides.length) {
-      slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+// /* Основная функция слайдера */
+// function showSlides(n) {
+//     let i;
+//     let slides = document.getElementsByClassName("item");
+//     let dots = document.getElementsByClassName("slider-dots_item");
+//     if (n > slides.length) {
+//       slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+// }
 
 
 // $(function() {
